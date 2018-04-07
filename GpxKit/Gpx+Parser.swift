1d8e880c334fd72b11894efd46f2a252bad20dc1
Applitom <applitom@gmail.com>
2018-04-07 18:11:05 +0300

    Adding GpxKit source as non submodule

---
+++ GpxKit/Gpx+Parser.swift
@@ -0,0 +1,68 @@
+//
+//  Copyright © 2017 Beeline. All rights reserved.
+//
+
+import CoreLocation
+import SWXMLHash
+
+public extension Gpx {
+
+    public init(data: Data) throws {
+        let gpx = SWXMLHash.parse(data)["gpx"]
+
+        self.creator = gpx["creator"].element?.text ?? ""
+
+        self.metadata = gpx["metadata"].metadata()
+
+        self.waypoints = try gpx["wpt"].all.map { waypoint in
+            try waypoint.point()
+        }
+
+        self.route = try gpx["rte"]["rtept"].all.map { routePoint in
+            try routePoint.point()
+        }
+
+        self.track = try gpx["trk"]["trkseg"].all.map { trackSegment in
+            try trackSegment["trkpt"].all.map { trackPoint in
+                try trackPoint.point()
+            }
+        }
+    }
+}
+
+fileprivate extension XMLIndexer {
+
+    func metadata() -> Metadata? {
+        guard element != nil else { return nil }
+        let name = self["name"].element?.text
+        let description = self["description"].element?.text
+        return Metadata(name: name, description: description)
+    }
+
+    func point() throws -> Point {
+        guard let element = element else { throw IndexingError.error }
+        let latitude = try element.attributeValue(by: "lat")
+        let longitude = try element.attributeValue(by: "lon")
+        let elevation = self["ele"].element?.text
+        let time = self["time"].element?.text
+        return Point(
+            CLLocationCoordinate2D(latitude: latitude, longitude: longitude),
+            elevation: elevation != nil ? Double(elevation!) : nil,
+            time: time != nil ? DateFormatter.iso8601.date(from: time!) : nil
+        )
+    }
+
+}
+
+fileprivate extension XMLElement {
+
+    func attributeValue(by name: String) throws -> Double {
+        guard let attribute = attribute(by: name) else {
+            throw IndexingError.attribute(attr: name)
+        }
+        guard let value = Double(attribute.text) else {
+            throw IndexingError.attributeValue(attr: name, value: attribute.text)
+        }
+        return value
+    }
+}
---
+++ GpxKit/Gpx.swift
@@ -0,0 +1,34 @@
+//
+//  Copyright © 2017 Beeline. All rights reserved.
+//
+
+import CoreLocation
+
+public struct Gpx {
+    public let creator: String
+    public let metadata: Metadata?
+    public let waypoints: [Point]?
+    public let route: [Point]?
+    public let track: [[Point]]?
+}
+
+public struct Metadata {
+    public let name: String?
+    public let description: String?
+}
+
+public struct Point {
+    public let coordinate: CLLocationCoordinate2D
+    public let elevation: Double?
+    public let time: Date?
+
+    public init(lat: Double, lon: Double, elevation: Double? = nil, time: Date? = nil) {
+        self.init(CLLocationCoordinate2DMake(lat, lon), elevation: elevation, time: time)
+    }
+
+    public init(_ coordinate: CLLocationCoordinate2D, elevation: Double? = nil, time: Date? = nil) {
+        self.coordinate = coordinate
+        self.elevation = elevation
+        self.time = time
+    }
+}
